1b2a7c29cea4c9b72d0c22bac57f97874df2646e
Taha Choudhary <tahachoudhary54@gmail.com>
2026-01-29 10:18:50 +0530

    first commit

---
+++ src/component/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        We are dedicated to providing the best service to our customers. Our team is passionate about delivering high-quality products and ensuring customer satisfaction.
+      </p>
+    </div>
+  );
+}
